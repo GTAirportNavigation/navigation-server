e034b47ac40e68194d456123dcb9bebddd84907e
William Broniec <williambroniec@gmail.com>
2018-01-30 12:21:43 -0500

    Node toString

--- nav/testing.py
+++ nav/testing.py
@@ -2,10 +2,13 @@ from domestic_terminal import Node, Dir, Type
 
 graph = []
 
-graph.append(Node('Tenter', 'Concourse T'))
-graph.append(Node('T8', 'Gate T10'))
-graph.append(Node('T9', 'Gate T9'))
+graph.append(Node('Tenter', 'Concourse T', Dir.E))
+graph.append(Node('T8', 'Gate T10', Dir.E))
+graph.append(Node('T9', 'Gate T9', Dir.E))
 
 # These Distance Values are Not Accurate
 graph[0].add_path('T8', 100, Dir.S, Type.NORMAL)
 graph[0].add_path('T9', 100, Dir.N, Type.NORMAL)
+
+for node in graph:
+    print(node)
