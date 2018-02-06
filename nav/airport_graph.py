88e8414d246596873d440f49fa948623284e0247
William Broniec <williambroniec@gmail.com>
2018-02-06 13:29:25 -0500

    Removed Direction
    Angle will be more accurate

--- nav/airport_graph.py
+++ nav/airport_graph.py
@@ -1,35 +1,21 @@
 import enum
 
-# Cardinal Direction
-class Dir(enum.Enum):
-    # N/A
-    X = 0
-    # North
-    N = 1
-    NE = 2
-    # South
-    E = 3
-    SE = 4
-    # East
-    S = 5
-    SW = 6
-    # West
-    W = 7
-    NW = 8
+## Angle Shortcuts Definitions ##
+NORTH = 0
+EAST = 90
+SOUTH = 180
+WEST = 270
 
 def opposite_directions(a, b):
-    av = a.value
-    bv = b.value
-    if av == 0 or bv == 0:
-        return False
-    if av > 4:
-        av -= 4
-        if av == bv:
+    if a >= 180:
+        a -= 180
+        if a == b:
             return True
     else:
-        av += 4
-        if av == bv:
+        a += 180
+        if a == b:
             return True
+    print(a, b)
     return False
 
 # Node Type
@@ -38,12 +24,12 @@ class Type(enum.Enum):
     # with no intrinsic value
     # but connects other nodes
     Joint = 0
-    Concourse = 1
     Gate = 2
     # Airline Counter
     Counter = 3
-    Restaurant = 4
-    Store = 5
+    Food = 4
+    Retail = 5
+    Club = 7
 
 # Graph Node
 class Node(object):
@@ -55,8 +41,8 @@ class Node(object):
         self.paths = []
     def __str__(self):
         return 'ID: {} Name: \'{}\' Type: {}\nPaths: {}'.format(self.id, self.name, self.type, self.paths)
-    def add_path(self, id, distance, dir, path_type):
-        self.paths.append((id, distance, dir, path_type))
+    def add_path(self, id, distance, angle, path_type):
+        self.paths.append((id, distance, angle, path_type))
 
 # Path Type
 class Path(enum.Enum):
@@ -87,7 +73,6 @@ def find_path(graph, start, end, constraints=[]):
     while len(open) > 0:
         temp = open.pop(0)
         tr = route.pop(0)
-        print('Iteration:',temp,tr)
         if graph[temp].id == end:
             tr.append(temp)
             return tr
@@ -99,6 +84,18 @@ def find_path(graph, start, end, constraints=[]):
                     open.append(index)
                     route.append(tr + [temp])
 
+# angle is the angle from node1 to node2
+def insert_path(graph, node1, node2, distance, angle, path_type=Path.NORMAL):
+    index1 = get_node_index(graph, node1)
+    index2 = get_node_index(graph, node2)
+    graph[index1].add_path(node2, distance, angle, path_type)
+    angle2 = 0
+    if angle >= 180:
+        angle2 = angle - 180
+    else:
+        angle2 = angle + 180
+    graph[index2].add_path(node1, distance, angle2, path_type)
+
 # Ensure All Edges Are Reversed
 # Also Checks Matching Distance Values
 # As Well As Opposing Directions
@@ -109,11 +106,11 @@ def verify_graph(graph):
         for path1 in node1.paths:
             eid = path1[0]
             distance = path1[1]
-            dir = path1[2]
+            angle = path1[2]
             node2 = get_node_index(graph, eid)
             verified = False
             for path2 in graph[node2].paths:
-                if path2[0] == sid and path2[1] == distance and opposite_directions(path2[2], dir):
+                if sid == path2[0] and distance == path2[1] and opposite_directions(angle, path2[2]):
                     verified = True
             if not verified:
                 print('Error:', sid, eid)
