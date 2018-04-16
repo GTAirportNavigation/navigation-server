bc22c90f18e194bb4ac0753822cff16cd71490ba
Artur Tarasenko <arttarawork@gmail.com>
2018-04-16 16:50:18 -0400

    Added get_gate_info function. Made get_on_time cleaner to read.

--- nav/utils.py
+++ nav/utils.py
@@ -76,7 +76,47 @@ def get_on_time(flightId):
 
 	estimatedarrivaltime = response0['flights'][flight]['estimatedarrivaltime']
 
-	return (((filed_departuretime + filedFlightLength) - estimatedarrivaltime) < 15*60)
+	onTime = (((filed_departuretime + filedFlightLength) - estimatedarrivaltime) < 15*60)
+
+	return onTime
+
+#returns a list containing [get_gate(flightId), get_on_time(flightId)]
+#this is made as [string, boolean]
+#this is a separate function, because it does what we need to do with one API call
+def get_gate_info(flightId):
+	init_flight_aware()
+
+	session = Session()
+	session.auth = HTTPBasicAuth(username, password)
+
+
+	transport = Transport(cache=SqliteCache(), session=session)
+	client = Client('http://flightxml.flightaware.com/soap/FlightXML2/wsdl', transport = transport)
+
+	response0 = zeep.helpers.serialize_object(client.service.FlightInfo(flightId, 15), target_cls=collections.OrderedDict)
+
+	flight = 0
+
+	while((flight < 14) and ((response0['flights'][flight + 1]['filed_departuretime'] - int(time.time())) > 0)):
+	    flight += 1
+
+	filed_departuretime = response0['flights'][flight]['filed_departuretime']
+
+	filed_ete = response0['flights'][flight]['filed_ete']
+
+	filedFlightLength = int(filed_ete[0:2]) * 3600 + int(filed_ete[3:5]) * 60 + int(filed_ete[6:8])
+
+	estimatedarrivaltime = response0['flights'][flight]['estimatedarrivaltime']
+
+	onTime = (((filed_departuretime + filedFlightLength) - estimatedarrivaltime) < 15*60)
+
+	faFlightID = zeep.helpers.serialize_object(client.service.GetFlightID(flightId, filed_departuretime), target_cls=collections.OrderedDict)
+
+	response1 = client.service.AirlineFlightInfo(faFlightID)
+
+	originGate = response1['gate_orig']
+
+	return [originGate, onTime]
 
 #returns the filed departure time as an integer in Unix Epoch time.
 #If presenting directly to users, it's necessary to convert this to normal human readable time.
